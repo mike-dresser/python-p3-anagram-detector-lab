47ece009e1632958cba5d04092b64b6081eeec7d
Mike Dresser <116307817+michaeldresser81@users.noreply.github.com>
2024-03-13 15:20:33 -0400

    completed

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,10 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word) -> None:
+        self.word = word
+
+    def match(self, match_list):
+        result = []
+        for word in match_list:
+            if sorted(self.word) == sorted(word):
+                result.append(word)
+        return result
